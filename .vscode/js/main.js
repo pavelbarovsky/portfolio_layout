684d5502caca7369e534101c4c070d2902468c83
pavel barovsky <pashazhar111@mail.ru>
2022-05-28 16:25:51 +0300

    перезалив

---
+++ .vscode/js/main.js
@@ -0,0 +1,9 @@
+$(window).on('load',function(){  /*запуск видео когда страница загружается по функции*/
+
+    //vide.js - video background
+    $('#header').vide('./video/cover', {
+        bgColor: '#4e534d'
+    })
+
+
+});  
